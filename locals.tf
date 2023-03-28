53493dc543bb61cca81844b2dcfc7634e67bd608
Curtis Slone <curtisdslone@gmail.com>
2023-03-28 11:34:00 +0200

    Added lower function to locals to remove case sensitivity from domain name label

--- locals.tf
+++ locals.tf
@@ -8,4 +8,5 @@
 locals {
   backup_resource_group_name = var.backup_policy_id != null ? split("/", var.backup_policy_id)[4] : null
   backup_recovery_vault_name = var.backup_policy_id != null ? split("/", var.backup_policy_id)[8] : null
+  domain_name_label = lower(coalesce(var.internal_dns_name_label, local.vm_name))
 }
